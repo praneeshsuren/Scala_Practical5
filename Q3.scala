f52931e5316306efbf5cae9994b99ced2163e8db
praneeshsuren <praneeshsuren@gmail.com>
2024-07-16 00:57:45 +0530

    feat: added code for Q3

---
+++ Q3.scala
@@ -0,0 +1,23 @@
+def fibonacci(n: Int): Seq[Int] = {
+    if (n <= 0) Seq()
+    else if (n == 1) Seq(0)
+    else if (n == 2) Seq(0, 1)
+    else {
+        var fibSeq = Seq(0, 1)
+        for (i <- 2 until n) {
+            fibSeq = fibSeq :+ fibSeq(i-1) + fibSeq(i-2)
+        }
+        fibSeq
+    }  
+}
+
+@main def Q3() : Unit = {
+    print("Enter the number of terms of Fibonacci series to display: ")
+    val n = scala.io.StdIn.readInt()
+    if(n <= 0) {
+        println("Invalid input")
+        return
+    }
+    val fibSeq = fibonacci(n)
+    println(fibSeq.mkString(", "))
+}
