24080f8dbf37552f96a3b640396108985c50f772
praneeshsuren <praneeshsuren@gmail.com>
2024-07-16 10:53:39 +0530

    fix: did some changes to improve code

--- Q2.scala
+++ Q2.scala
@@ -1,115 +1,102 @@
-class Book{
-    var title: String = ""
-    var author: String = ""
-    var isbn: String = ""
-}
+import scala.io.StdIn.readLine
+import scala.io.StdIn.readInt
 
-var books = Array[Book]()
+case class Book(title: String, author: String, isbn: String)
+
+var books: Set[Book] = Set()
 
 def addBook(): Unit = {
-    var book = new Book()
-    print("Enter Title: ")
-    book.title = scala.io.StdIn.readLine()
-    print("Enter Author: ")
-    book.author = scala.io.StdIn.readLine()
-    print("Enter ISBN: ")
-    book.isbn = scala.io.StdIn.readLine()
-    println()
-    books = books :+ book
+  print("Enter Title: ")
+  val title = readLine()
+  print("Enter Author: ")
+  val author = readLine()
+  print("Enter ISBN: ")
+  val isbn = readLine()
+  println()
+  val book = Book(title, author, isbn)
+  books += book
 }
 
 def removeBook(isbn: String): Unit = {
-    var index = books.indexWhere(_.isbn == isbn)
-    if (index != -1) {
-        books = books.filter(_.isbn != isbn)
-    } else {
-        println("Book not found")
-    }
+  val initialSize = books.size
+  books = books.filterNot(_.isbn == isbn)
+  if (initialSize == books.size) {
+    println("Book not found")
+  }
 }
 
 def displayBooks(): Unit = {
-    println("Title\t\t\tAuthor\t\t\tISBN")
-    for (i<-0 until books.length) {
-        println(books(i).title + "\t\t" + books(i).author + "\t\t" + books(i).isbn)
-    }
+  println("Title\t\t\tAuthor\t\t\tISBN")
+  books.foreach(book => println(s"${book.title}\t\t${book.author}\t\t${book.isbn}"))
 }
 
 def checkBook(isbn: String): Unit = {
-    var index = books.indexWhere(_.isbn == isbn)
-    if (index != -1) {
-        println("Book exists")
-        println("Title: " + books(index).title)
-        println("Author: " + books(index).author)
-        println("ISBN: " + books(index).isbn)
-    }
-    else{
-        print("Book does not exist")
-    }
+  books.find(_.isbn == isbn) match {
+    case Some(book) =>
+      println("Book exists")
+      println(s"Title: ${book.title}")
+      println(s"Author: ${book.author}")
+      println(s"ISBN: ${book.isbn}")
+    case None => println("Book does not exist")
+  }
 }
 
 def searchBook(name: String): Unit = {
-    var index = books.indexWhere(_.title == name)
-    if (index != -1) {
-        println("Title\t\t\tAuthor\t\t\tISBN")
-        println(books(index).title + "\t\t" + books(index).author + "\t\t" + books(index).isbn)
-    } else {
-        println("Book not found")
-    }
+  books.find(_.title.equalsIgnoreCase(name)) match {
+    case Some(book) =>
+      println("Title\t\t\tAuthor\t\t\tISBN")
+      println(s"${book.title}\t\t${book.author}\t\t${book.isbn}")
+    case None => println("Book not found")
+  }
 }
 
 def displayBookByAuthor(name: String): Unit = {
-    var booksByAuthor = books.filter(_.author == name)
-    if (booksByAuthor.length != 0) {
-        println("Title\t\t\tAuthor\t\t\tISBN")
-        for (i<-0 until booksByAuthor.length) {
-            println(booksByAuthor(i).title + "\t\t" + booksByAuthor(i).author + "\t\t" + booksByAuthor(i).isbn)
-        }
-    } else {
-        println("Book not found")
-    }
+  val booksByAuthor = books.filter(_.author.equalsIgnoreCase(name))
+  if (booksByAuthor.nonEmpty) {
+    println("Title\t\t\tAuthor\t\t\tISBN")
+    booksByAuthor.foreach(book => println(s"${book.title}\t\t${book.author}\t\t${book.isbn}"))
+  } else {
+    println("Book not found")
+  }
 }
 
-@main def Q2() : Unit = {
-    println("***********Welcome to the Library Management System*********")
-    var continue = true
-    while(continue){
-        println("Enter 1 to display books")
-        println("Enter 2 to add book")
-        println("Enter 3 to remove book")
-        println("Enter 4 to check book")
-        println("Enter 5 to search book")
-        println("Enter 6 to display book by author")
-        println("Enter 7 to exit")
-        print("Enter your choice: ")
-        var choice = scala.io.StdIn.readInt()
-        println() 
-        choice match {
-            case 1 => displayBooks()
-            case 2 => addBook()
-            case 3 => {
-                print("Enter ISBN: ")
-                var isbn = scala.io.StdIn.readLine()
-                removeBook(isbn)
-            }
-            case 4 => {
-                print("Enter ISBN: ")
-                var isbn = scala.io.StdIn.readLine()
-                checkBook(isbn)
-            }
-            case 5 => {
-                print("Enter Title: ")
-                var title = scala.io.StdIn.readLine()
-                searchBook(title)
-            }
-            case 6 => {
-                print("Enter Author: ")
-                var author = scala.io.StdIn.readLine()
-                displayBookByAuthor(author)
-            }
-            case 7 => continue = false
-            case _ => println("Invalid choice")
-        }
-        println()
+@main def Q2(): Unit = {
+  println("***********Welcome to the Library Management System*********")
+  var continue = true
+  while (continue) {
+    println("Enter 1 to display books")
+    println("Enter 2 to add book")
+    println("Enter 3 to remove book")
+    println("Enter 4 to check book")
+    println("Enter 5 to search book")
+    println("Enter 6 to display book by author")
+    println("Enter 7 to exit")
+    print("Enter your choice: ")
+    val choice = readInt()
+    println()
+    choice match {
+      case 1 => displayBooks()
+      case 2 => addBook()
+      case 3 =>
+        print("Enter ISBN: ")
+        val isbn = readLine()
+        removeBook(isbn)
+      case 4 =>
+        print("Enter ISBN: ")
+        val isbn = readLine()
+        checkBook(isbn)
+      case 5 =>
+        print("Enter Title: ")
+        val title = readLine()
+        searchBook(title)
+      case 6 =>
+        print("Enter Author: ")
+        val author = readLine()
+        displayBookByAuthor(author)
+      case 7 => continue = false
+      case _ => println("Invalid choice")
     }
-    println("Exiting...");
+    println()
+  }
+  println("Exiting...")
 }
